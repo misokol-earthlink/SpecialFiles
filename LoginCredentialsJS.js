41452bea779dc935d794024f2c8af236ef91d1ba
misokol-earthlink <102489949+misokol-earthlink@users.noreply.github.com>
2026-05-14 15:26:13 -0500

    Update LoginCredentialsJS.js

--- LoginCredentialsJS.js
+++ LoginCredentialsJS.js
@@ -95,7 +95,7 @@ function detectAdminStatus() {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  testAdmin();
+  //testAdmin();
   userName = detectLoginStatus();
   if (userName != "Guest" ) {
    sortedUsers = expandConcatenatedNames(fulldata) ;
